807d374655fb80c2eba5f048147622987d76601f
Kirti Chaudhari <147939730+Kirti-Chaudhari-1027@users.noreply.github.com>
2024-05-08 22:22:18 +0530

    Update p1.cpp

--- p1.cpp
+++ p1.cpp
@@ -1,6 +1,45 @@
 #include<iostream>
 using namespace std;
 
+void BinarySearch(int arr[],int key,int s,int e){
+
+  int mid = (s+e)/2;
+
+  if(arr[mid] == key){
+    cout<<"Found: "<<key<<" is present at "<<i<<" index"<<endl;
+  }
+  elseif(arr[mid] < key){
+    s = 0;
+    e = mid-1;
+    BinarySearch(arr,key);
+  }
+  elseif(arr[mid] > key){
+    s = mid+1;
+    e = n-1;
+    BinarySearch(arr,key);
+  }
+  else{
+    cout<<"Not found"<<endl;
+  }
+}
+
 int main(){
-  cout<<"Hello World"<<endl;
+  int n;
+  cout<<"\nEnter the no. of elements = ";
+  cin>>n;
+
+  int arr[n];
+  cout<<"\nEnter the array of elements = ";
+  for(int i=0;i<n;i++){
+    cin>>arr[i];
+  }
+
+  int key;
+  cout<<"\nEnter the key = ";
+  cin>>key;
+
+  int s=0;
+  int e=n-1;
+
+  BinarySearch(arr,key,s,e);
 }
